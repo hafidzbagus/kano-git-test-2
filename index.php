f1bbfb519d6d96db34088d5b9f2d735fc9c3bcc3
Hafidz Bagus <hafidzg04@gmail.com>
2020-12-03 13:54:30 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, voluptatibus!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
 </body>
 </html>
